703ac73512559f84b418e154c545c20d240503f9
Sampreet Patil <sampreetpatil270@gmail.com>
2024-12-20 22:46:26 +0530

    prac added or updated for the day

--- prac.py
+++ prac.py
@@ -1,20 +1,8 @@
-# Program to measure central tendency and measures of dispersion: Mean, Median, Mode, Standard Deviattion, Variance, Mean deviation and Quartile deviation for a frequency distribution/data
-
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
-def calculate_statistics(data, frequencies):
-    df = pd.DataFrame({'Value': data, 'Frequency': frequencies})
-
-    total = df['Frequency'].sum()
-
-    df['Weighted_Value'] = df['Value'] * df['Frequency']
-    mean = df['Weighted_Value'].sum() / total
-
-    cumulative_frequency = df['Frequency'].cumsum()
-    median_index = cumulative_frequency.searchsorted(total / 2)
-    median = df['Value'][median_index]
-
-    mode = df['Value'][df['Frequency'].idxmax()]
-
-    variance = np.average
+# Create sample test series data
+np.random.seed(42)
+data_range
